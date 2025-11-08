9aa8bd9b4d41bdf2257f16ad068886cbc250355f
BuildTools <unconfigured@null.spigotmc.org>
2025-11-08 11:56:46 +0100

    some update

--- gameClass.py
+++ gameClass.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from playerClass import Player
 from plantsClass import Plant
 from entityClass import Entity, Lawnmoyer, LivingPlant
+from time import time
 
 class Game(Tk):
     def __init__(self, 
@@ -17,6 +18,7 @@ class Game(Tk):
         self.board_width = board_width
         self.waves = []
         self.board = []
+        self.speed = 1
     
     def set_waves(self, waves: dict[int, list[Plant]] | None):
         if waves:
@@ -25,30 +27,42 @@ class Game(Tk):
             self.waves = []
     
     def draw(self):
-        board_frame = Frame(self, padx=10, pady=10, bg='grey')
+        board_frame = Frame(self, bg='grey')
         board_frame.grid(row=0, column=1)
+        self.rowconfigure(list(range(0, self.board_height, 2)), pad=10)
         
         board: list[list[Slot]] = []
         for y in range(self.board_height):
             board.append([])
             for x in range(self.board_width):
-                slot = Slot(board_frame)
+                slot = Slot(board_frame, x, y, (x, y))
                 slot.grid(column=x, row=y)
                 board[y].append(slot)
         self.board = board
         
+        deck_frame = Frame(self, bg='grey')
+        deck_frame.grid()
+        
+        self.after(1, lambda: self.tick(time()))
         self.mainloop()
     
-    def tick(self):
-        pass
+    def tick(self, last_tick):
+        self.after(1, lambda: self.tick(time()))
+        
 
 class Slot(Button):
     def __init__(self,
                  master: Frame,
+                 x: int,
+                 y: int,
+                 pos: tuple,
                  taken_by: LivingPlant | None = None,
                  entities: list[Entity | Lawnmoyer | None] = []):
         super().__init__()
         self.master = master
         self.taken_by = taken_by
         self.entities = entities
+        self.x, self.y = x, y
+        self.pos = pos
+        
         self.configure(bg="green", width=10, height=3)
--- playerClass.py
+++ playerClass.py
@@ -1,6 +1,7 @@
-from dataclasses import dataclass
-from common import Lane
+from dataclasses import dataclass, field
+from plantsClass import Plant, PEASHOOTER
 
 @dataclass
 class Player:
+    unlocked_plants: list[Plant] = field(default_factory=lambda: [PEASHOOTER])
     suns: int = 0
